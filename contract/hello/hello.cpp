936f88326e9340e16a131a96d57cef25ea1b4346
ChenDiagram <dchen499@gmail.com>
2022-08-07 18:46:43 +0000

    Accounts created

---
+++ contract/hello/hello.cpp
@@ -0,0 +1,10 @@
+#include <eosio/eosio.hpp>
+using namespace eosio;
+class [[eosio::contract]] hello : public contract {
+  public:
+      using contract::contract;
+      [[eosio::action]]
+      void hi( name user ) {
+         print( "Hello, ", user);
+      }
+};
